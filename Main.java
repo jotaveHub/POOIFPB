f5481c39f793c48855f4693f06543c04e9825c4b
jotaveHub <accurategx@gmail.com>
2026-03-09 20:24:48 -0300

    Adicionando Repositório Git da disciplina de Progamação Orientada a Objetos

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Informe o seu nome: ");
+		String nome = scanner.nextLine();
+		System.out.printf("Bem vindo,  %s\n", nome);
+		System.out.print("Digite a sua idade: ");
+		int idade = scanner.nextInt();
+		scanner.nextLine();
+		System.out.print("Qual a sua profissão? ");
+		String profissao = scanner.nextLine();
+		
+		System.out.println(" Bem vindo,"+nome+"!\n");
+		System.out.println("Você é "+profissao+" e tem "+idade+" anos.");
+		
+	}
+}
