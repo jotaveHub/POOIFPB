7919eb13cf3143ce99a222136409e3294603b1a6
jotaveHub <accurategx@gmail.com>
2026-03-11 19:51:05 -0300

    Fazendo algumas alterações, tentando resolver config do ambiente.

--- src/Ex01.java
+++ src/Ex01.java
@@ -1,22 +1,11 @@
-void main() {
-    double a = 4.0;
-    double b = 2.0;
-    double c = 1.0;
+//Crie um programa que imprime todos os números múltiplos de 7 entre 0 e 1000.
 
-    double Delta = Math.pow(b,2) - 4*a*c;
-    double x1 = ((b * -1) + Math.sqrt(Delta)) / (2 * a);
-    double x2 = ((b * -1) - Math.sqrt(Delta)) / (2 * a);
+public class Ex01 {
+    public static void Ex01(String[] args) {
+        int acumulador = 0;
 
-    if(Delta < 0) {
-        System.out.println("Não possui raízes reais.");
-    }
-    if(Delta == 0) {
-        System.out.println("Só possui uma raiz: %.2f"+(-b) / (2*a));
-    }
-    if(Delta > 0) {
-    System.out.println(Delta);
-    System.out.println(x1);
-    System.out.println(x2);
-    }
+        for(int k = 0; k <= 1000; k++) {
 
+        }
+    }
 }
