167c2c23beca121cf551f35deca2b5b88d0dec13
jotaveHub <accurategx@gmail.com>
2026-03-11 20:55:51 -0300

    Resolvendo Exercício01

--- src/Ex01.java
+++ src/Ex01.java
@@ -1,11 +1,14 @@
 //Crie um programa que imprime todos os números múltiplos de 7 entre 0 e 1000.
 
 public class Ex01 {
-    public static void Ex01(String[] args) {
+    public static void main(String[] args) {
         int acumulador = 0;
 
         for(int k = 0; k <= 1000; k++) {
-
+            if(k % 7 == 0) {
+                acumulador += k;
+                System.out.println(acumulador);
+            }
         }
     }
 }
---
+++ src/Ex03.java
@@ -0,0 +1,9 @@
+package src;/*Crie um método que recebe um número inteiro e verifica se ele é um número perfeito;
+Número perfeito é aquele cujo valor é igual à soma de todos seus divisores próprios.*/
+
+
+public class Ex03 {
+    public static void main (String[] args) {
+
+    }
+}
