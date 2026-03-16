1a45ad3f7cd12ef62cb77ad80622b84453c76e9d
jotaveHub <accurategx@gmail.com>
2026-03-16 09:05:53 -0300

    Resolvendo Exercício06&04

--- src/Ex04.java
+++ src/Ex04.java
@@ -1,4 +1,7 @@
 package src;
 
 public class Ex04 {
+    public static void main() {
+
+    }
 }
--- src/Ex06.java
+++ src/Ex06.java
@@ -1,4 +1,16 @@
 package src;
 
 public class Ex06 {
+    public static void main (String[] args) {
+        String senha_forte = "123456";
+
+        for(int i = 0; i<3; i++) {
+            String senha = IO.readln("Informe a senha:");
+            if(senha.equals(senha_forte)) {
+                IO.println(("Bem-Vindo"));
+                break;
+            }
+        }
+        IO.println("Senha Incorreta, tente novamente");
+    }
 }
