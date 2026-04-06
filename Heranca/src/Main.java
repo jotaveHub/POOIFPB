6d288490e72fc6a18bae50aa0a34431747877b2b
jotaveHub <accurategx@gmail.com>
2026-04-06 08:35:34 -0300

    Feat: Criando main

---
+++ Heranca/src/Main.java
@@ -0,0 +1,22 @@
+import modelo.Aluno;
+import modelo.Pessoa;
+import modelo.Professor;
+
+void main() {
+    /*
+    Pessoa pessoa = new Pessoa("João",111.111.111-01,
+            LocalDate.of(2000,01,01),"Rua X, 30, Cajazeiras");
+
+    Aluno aluno = new Aluno("Maria","222.222.222-22",LocalDate.of(2000,4,7),
+            123, "Rua X, 123, Sousa-PB","ADS");
+
+    Professor professor = new Professor("José","333.333.333-33",LocalDate.of(2002,8,14),
+            1234,"Rua Gx, 45, Jardim Oasis",2000f); */
+
+    Pessoa pessoa = new Aluno("Maria","222.222.222-22",LocalDate.of(2000,4,7),
+            123, "Rua X, 123, Sousa-PB","ADS");
+
+    Pessoa pessoa1 = new Professor("José","333.333.333-33",LocalDate.of(2002,8,14),
+            1234,"Rua Gx, 45, Jardim Oasis",2000f);
+
+}
