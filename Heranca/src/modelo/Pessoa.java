9075381fda08924efd8a9fd6ee6ed8ea6bf49c92
jotaveHub <accurategx@gmail.com>
2026-04-06 08:08:49 -0300

    Fix: ajustando todas as classes

--- Heranca/src/modelo/Pessoa.java
+++ Heranca/src/modelo/Pessoa.java
@@ -1,4 +1,17 @@
 package modelo;
+import java.time.LocalDate;
 
 public class Pessoa {
+    private String nome;
+    private String cpf;
+    private LocalDate nascimento;
+    private String endereco;
+
+    public Pessoa(String nome, String cpf, LocalDate nascimento, String endereco) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.nascimento = nascimento;
+        this.endereco = endereco;
+    }
+
 }
--- Heranca/src/modelo/Professor.java
+++ Heranca/src/modelo/Professor.java
@@ -1,45 +1,14 @@
 package modelo;
 import java.time.LocalDate;
 
-public class Professor {
-    private String nome;
-    private String cpf;
-    private LocalDate nascimento;
+public class Professor extends Pessoa {
     private int matricula;
-    private String endereço;
-    private float Salário;
+    private float salario;
 
-    public Professor(String nome, String cpf, LocalDate nascimento, int matricula, String endereço, float salário) {
-        this.nome = nome;
-        this.cpf = cpf;
-        this.nascimento = nascimento;
+    public Professor(String nome, String cpf, LocalDate nascimento, int matricula, String endereco, float salário) {
+        super(nome,cpf,nascimento,endereco);
         this.matricula = matricula;
-        this.endereço = endereço;
-        Salário = salário;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public LocalDate getNascimento() {
-        return nascimento;
-    }
-
-    public void setNascimento(LocalDate nascimento) {
-        this.nascimento = nascimento;
+        this.salario = salário;
     }
 
     public int getMatricula() {
@@ -50,19 +19,11 @@ public class Professor {
         this.matricula = matricula;
     }
 
-    public String getEndereço() {
-        return endereço;
+    public float getsalario() {
+        return salario;
     }
 
-    public void setEndereço(String endereço) {
-        this.endereço = endereço;
-    }
-
-    public float getSalário() {
-        return Salário;
-    }
-
-    public void setSalário(float salário) {
-        Salário = salário;
+    public void setSalário(float salario) {
+        this.salario = salario;
     }
 }
