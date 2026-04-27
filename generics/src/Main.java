d7f44119b2aa20ecdec9f2dd9fda3206ad79c6d1
jotaveHub <accurategx@gmail.com>
2026-04-27 08:58:11 -0300

    feat: criando classe genérica e implementando na main

---
+++ generics/src/Main.java
@@ -0,0 +1,13 @@
+import modelo.Caixa;
+
+void main() {
+        /*
+        Caixa<String> caixaString = new Caixa<>();
+        caixaString.setObjeto("Olá, generics!");
+        System.out.println(caixaString.getObjeto());
+
+        Caixa<Integer> caixaInteger = new Caixa<>();
+        caixaInteger.setObjeto(42);
+        System.out.println(caixaInteger.getObjeto());*/
+
+    }
---
+++ generics/src/modelo/Caixa.java
@@ -0,0 +1,14 @@
+package modelo;
+
+public class Caixa<T> {
+
+    private T objeto;
+
+    public T getObjeto() {
+        return objeto;
+    }
+
+    public void setObjeto(T objeto) {
+        this.objeto = objeto;
+    }
+}
