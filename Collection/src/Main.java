c653cf416603f5f8a4dc9c54d2a6f37d97742777
jotaveHub <accurategx@gmail.com>
2026-04-29 00:21:19 -0300

    feat: Implementando métodos de ArrayList na main

--- Collection/src/Main.java
+++ Collection/src/Main.java
@@ -1,13 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+    List<String> lista = new ArrayList<>();
+    lista.add("João");
+    lista.add("Maria");
+    lista.add("Pedro");
+    //add Adiciona deslocando as posições correspondente
+    lista.add(1,"Ana");
+    //set Substitui na posição
+    lista.set(0, "Teste");
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+    lista.add(4,"Joaquim");
+    System.out.println(lista);
+    System.out.println("Tamanho:" + lista.size());
+
+    lista.remove("Teste");
+    System.out.println(lista);
+    System.out.println("Tamanho:" + lista.size());
+
+    System.out.println("Posição 0?" + lista.get(0));
+    System.out.println("Lista contém João?" + lista.contains("João"));
+
+    for(String s: lista) {
+        System.out.println(s);
     }
 }
