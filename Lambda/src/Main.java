7d794cb8cb37642dfb84d4fc80f85e863d82de47
jotaveHub <accurategx@gmail.com>
2026-05-06 08:30:30 -0300

    feat: Usando classe interna e método comparator

--- Lambda/src/Main.java
+++ Lambda/src/Main.java
@@ -2,14 +2,28 @@ import model.Pessoa;
 
 void main() {
     List<Pessoa> pessoas = new ArrayList<>();
-    pessoas.add((new Pessoa("João",LocalDate.of(2003,10,4))));
-    pessoas.add(new Pessoa("José",LocalDate.of(2004,8,5)));
-    pessoas.add(new Pessoa("Maria",LocalDate.of(2000,6,24)));
+    pessoas.add((new Pessoa("João", LocalDate.of(2003, 10, 4))));
+    pessoas.add(new Pessoa("José", LocalDate.of(2004, 8, 5)));
+    pessoas.add(new Pessoa("Maria", LocalDate.of(2000, 6, 24)));
+    pessoas.add(new Pessoa("Ana", LocalDate.of(1999, 9, 8)));
 
+    ComparadorPessoaNome comparador = new ComparadorPessoaNome();
+    Collections.sort(pessoas, comparador);
+    System.out.println(pessoas);
+
+    Collections.sort(pessoas, new Comparator<Pessoa>() {
+        @Override
+        public int compare(Pessoa o1, Pessoa o2) {
+            return o1.getNome().compareTo(o2.getNome());
+        }
+    });
+}
+
+
+    //classe interna
     class ComparadorPessoaNome implements Comparator<Pessoa>{
         @Override
         public int compare(Pessoa o1, Pessoa o2) {
             return o1.getNome().compareTo(o2.getNome());
         }
-    }
 }
