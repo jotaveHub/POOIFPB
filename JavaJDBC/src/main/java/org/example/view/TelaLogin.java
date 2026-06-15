ce273189a9abf89b04dd1b00e29a393b8144afbf
jotaveHub <accurategx@gmail.com>
2026-06-15 08:33:59 -0300

    Feat: Criando tela de login Usando Swing

---
+++ JavaJDBC/src/main/java/org/example/view/TelaLogin.java
@@ -0,0 +1,36 @@
+package org.example.view;
+
+import org.example.dao.UsuarioDaoJdbc;
+
+import javax.swing.*;
+
+public class TelaLogin extends JFrame {
+    private JPanel contentPane;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JLabel label1;
+    private UsuarioDaoJdbc usuarioDao;
+
+    public TelaLogin() {
+        usuarioDao = new UsuarioDaoJdbc();
+        setContentPane(contentPane);
+        getRootPane().setDefaultButton(buttonOK);
+    }
+
+    public static void main(String[] args) {
+        TelaLogin dialog = new TelaLogin();
+        dialog.pack();
+        dialog.setVisible(true);
+
+    }
+
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+         label1 = new JLabel();
+        ImageIcon icone = new ImageIcon("src/main/java/org/example/img/Login_37128.png");
+        label1.setIcon(icone);
+    }
+}
